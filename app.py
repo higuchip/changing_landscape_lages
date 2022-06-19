5e66309259cff2c8a07e3682100b752f5214ede4
higuchip <higuchip@gmail.com>
2022-06-19 09:33:19 -0300

    fix

--- app.py
+++ app.py
@@ -52,7 +52,35 @@ mapbiomas = ee.Image(
 
 # Seleção de anos
 
-antes = st.slider('Selecione o antes?', 1985:2020)
+antes = st.slider(
+    "Selecione o antes?",
+    1985,
+    1986,
+    1987,
+    1988,
+    1989,
+    2000,
+    2001,
+    2002,
+    2003,
+    2004,
+    2005,
+    2006,
+    2007,
+    2008,
+    2009,
+    2010,
+    2011,
+    2012,
+    2013,
+    2014,
+    2015,
+    2016,
+    2017,
+    2018,
+    2019,
+    2020,
+)
 st.write("You selected:", antes)
 
 mbyr_2010 = mapbiomas.select("classification_2010")

0dfcb25e3bc3a45e226ee63fca9c5c37f3c7cb41
higuchip <higuchip@gmail.com>
2022-06-19 09:21:52 -0300

    fix

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ st.markdown("___")
 
 
 Map = geemap.Map()
-Map.add_basemap("SATELLITE")
-Map.to_streamlit(height=600)
+
 
 # Autenticar e Inicializar EE
 # ee.Initialize()
@@ -179,7 +178,7 @@ left_layer = geemap.ee_tile_layer(
 right_layer = geemap.ee_tile_layer(
     mbyr_2020, {"min": 0, "max": 49, "palette": legend_colors}, name="Lages 2022"
 )
-Map = geemap.Map()
+
 Map.add_basemap("SATELLITE")
 Map.split_map(left_layer, right_layer)
 Map.add_legend(legend_title="Classes", legend_dict=legend_dict)
